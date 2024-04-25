35702a325ac82451e801a25da6c9cb8527e7334f
alexmiloeski <a.miloeski@gmail.com>
2024-04-25 10:20:23 +0200

    add test 'add' method

--- src/main/java/com/example/calc/CalcApplication.java
+++ src/main/java/com/example/calc/CalcApplication.java
@@ -8,6 +8,13 @@ public class CalcApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CalcApplication.class, args);
+
+		int res = add(3, 5);
+		System.out.println("res = " + res);
+	}
+
+	static int add(int x, int y) {
+		return x + y;
 	}
 
 }
